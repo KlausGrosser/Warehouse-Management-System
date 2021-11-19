836b2e884f1fbf1fcf2d970d4f6e38dc0cd701e3
KlausGrosser <klaus.grosser@deliveryhero.com>
2021-11-19 19:32:33 +0100

    Version3

--- src/main/java/intro/TheWarehouseManager.java
+++ src/main/java/intro/TheWarehouseManager.java
@@ -1,18 +1,28 @@
 package main.java.intro;
 
+
+import main.java.data.Item;
+import main.java.data.Person;
+import main.java.data.StockRepository;
+
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+
+import static main.java.data.PersonnelRepository.getAllPersons;
+import static main.java.data.PersonnelRepository.isUserValid;
 
 /**
  * Provides necessary methods to deal through the Warehouse management actions
  *
  * @author riteshp
  */
-public class TheWarehouseManager extends Repository {
+public class TheWarehouseManager extends StockRepository {
     // =====================================================================================
     // Member Variables
     // =====================================================================================
+    private static final Person currentUser = new Person();
 
+    private static final List<String> SESSION_ACTIONS = new ArrayList<>();
     // To read inputs from the console/CLI
     private final Scanner reader = new Scanner(System.in);
     private final String[] userOptions = {
@@ -20,6 +30,7 @@ public class TheWarehouseManager extends Repository {
     };
     // To refer the user provided name.
     private String userName;
+
 
     // =====================================================================================
     // Public Member Methods
@@ -29,9 +40,19 @@ public class TheWarehouseManager extends Repository {
      * Welcome User
      */
     public void welcomeUser() {
-        this.seekUserName();
+        this.statusCheck();
         this.greetUser();
+        this.authorizations();
     }
+public void statusCheck() {
+if(this.confirm("Welcome to SWIP Warehouse Management System.\nAre you an employee? ")){
+    this.seekUserName();
+    this.seekPassword();
+}else{
+    currentUser.setUserName("Guest");
+}
+    }
+
 
     /**
      * Ask for user's choice of action
@@ -50,7 +71,7 @@ public class TheWarehouseManager extends Repository {
                 while(!reader.hasNextInt()) { //repeat until a number is entered.
                    String input = reader.nextLine();
                     System.out.println("\n**************************************************\n" +
-                            input + " is not a valid operation. Please enter a number between 1 and 4!\n" +
+                            input + " is not a valid operation. Please enter a number between 1 and " +this.userOptions.length+"!\n" +
                             "**************************************************");
                     System.out.print("Type the number of the operation: ");
                 }
@@ -105,11 +126,25 @@ public class TheWarehouseManager extends Repository {
         return choice.startsWith("y");
     }
 
+    private static void listSessionActions() {
+        int i = 0;
+        if(SESSION_ACTIONS.size() < 1){
+            System.out.println("In this session you have not done anything.");
+        }else {
+            System.out.println("In this session you have:");
+            for (String action : SESSION_ACTIONS) {
+                i++;
+                System.out.println("        "+i +". "+action);
+            }
+        }
+    }
+
     /**
      * End the application
      */
     public void quit() {
-        System.out.printf("\nThank you for your visit, %s!\n", this.userName);
+        System.out.printf("\nThank you for your visit, %s!\n", currentUser.getUserName());
+        listSessionActions();
         System.exit(0);
     }
 
@@ -117,36 +152,58 @@ public class TheWarehouseManager extends Repository {
     // Private Methods
     // =====================================================================================
 
+    private boolean verifyUser(){
+        return isUserValid(currentUser.getUserName(), currentUser.getPassword());
+    }
+
+    private void authorizations() {
+        if(this.verifyUser()){
+            System.out.println("You are authorized to place an order.");
+        }else if(currentUser.getUserName().equals("Guest")){
+            System.out.println("You are not authorized to place an order but you can still use the rest of the operations.");
+        }else{
+            System.out.println("Credentials not recognized. You are not authorized to place an order but you can still use the rest of the operations.");
+        }
+    }
     /**
      * Get user's name via CLI
      */
     private void seekUserName() {
         System.out.print("What is your user name? ");
-        this.userName = this.reader.nextLine();
+        currentUser.setUserName(this.reader.nextLine());
+    }
+
+    private void seekPassword() {
+        System.out.print("Please enter your password: ");
+        currentUser.setPassword(reader.nextLine());
     }
 
     /**
      * Print a welcome message with the given user's name
      */
     private void greetUser() {
-        System.out.printf("\nHello, %s!\n", this.userName);
+        System.out.printf("\nHello, %s!\n", currentUser.getUserName());
+    }
+
+    private void printNumberOfItemsByWarehouse() {
+        System.out.println();
+        for (int warehouse : getWarehouses()) {
+            System.out.printf("Total items in warehouse %d: %d\n", warehouse, StockRepository.getItemsByWarehouse(warehouse).size());
+        }
     }
 
     private void listItemsByWarehouse() {
         Set<Integer> warehouseNumbers = getWarehouses();
         for (int warehouse : warehouseNumbers) {
             System.out.printf("\nItems in warehouse %d:%n",warehouse);
-            this.listItems(Repository.getItemsByWarehouse(warehouse));
+            this.listItems(StockRepository.getItemsByWarehouse(warehouse));
+
         }
         printNumberOfItemsByWarehouse();
+        SESSION_ACTIONS.add("Listed "+getTotalListedItems(StockRepository.getAllItems())+" items.");
+
     }
 
-    private void printNumberOfItemsByWarehouse() {
-        System.out.println();
-        for (int warehouse : getWarehouses()) {
-            System.out.printf("Total items in warehouse %d: %d\n", warehouse, Repository.getItemsByWarehouse(warehouse).size());
-        }
-    }
 
     private void listItems(List<Item> warehouse) {
         for (Item item : warehouse) {
@@ -154,15 +211,45 @@ public class TheWarehouseManager extends Repository {
         }
     }
 
+    private int getTotalListedItems(List<Item> masterList){
+        return masterList.size();
+    }
+
 
     private void searchItemAndPlaceOrder() {
         String itemName = askItemToOrder();
-        printAmountAvailable(itemName);
-        printLocations(itemName);
-        if (getAvailableAmount(itemName) > 0) {
-            printMaximumAvailability(itemName);
-            askAmountAndConfirmOrder(getAvailableAmount(itemName), itemName);
+        printAmountAvailable(itemName.toLowerCase());
+        printLocations(itemName.toLowerCase());
+        if (getAvailableAmount(itemName.toLowerCase()) > 0) {
+            printMaximumAvailability(itemName.toLowerCase());
+            SESSION_ACTIONS.add("Searched "+getAppropriateIndefiniteArticle(formattedItem(itemName))+ formattedItem(itemName)+".");
+            askAmountAndConfirmOrder(getAvailableAmount(itemName.toLowerCase()), itemName);
+
         }
+
+    }
+
+
+    private boolean hasVowel(String itemName) {
+        switch (itemName.charAt(0)) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private String getAppropriateIndefiniteArticle(String itemName){
+            return (hasVowel(itemName))?"an " : "a ";
     }
 
     private void printMaximumAvailability(String item) {
@@ -177,7 +264,7 @@ public class TheWarehouseManager extends Repository {
             System.out.printf("Maximum availability: %d in each Warehouse\n", itemsInW1);
         }
   }
-//
+
     private void printAmountAvailable(String item) {
       System.out.println("Amount available: " + getAvailableAmount(item));
     }
@@ -189,7 +276,7 @@ public class TheWarehouseManager extends Repository {
      */
     private String askItemToOrder() {
         System.out.print("\nWhat is the name of the item? : ");
-        return reader.nextLine().toLowerCase().trim();
+        return reader.nextLine().trim();
     }
 
     /**
@@ -250,16 +337,41 @@ public class TheWarehouseManager extends Repository {
      * Ask order amount and confirm order
      */
     private void askAmountAndConfirmOrder(int availableAmount, String item) {
-
+        if(this.verifyUser()){
         boolean toOrder = this.confirm("\nWould you like to order this item? ");
         if (toOrder) {
-            int orderAmount = this.getOrderAmount(availableAmount);
-            if(orderAmount == 0){
-                return;
-            } else {
-                System.out.printf("%d %s%s been ordered\n", orderAmount, item, (orderAmount == 1 ? " has" : "s have"));
-                //System.out.println("Please try again");
+                int orderAmount = this.getOrderAmount(availableAmount);
+                if(orderAmount == 0){
+                    return;
+                }else{
+                    System.out.printf("%d %s%s been ordered\n", orderAmount, formattedItem(item), (orderAmount == 1 ? " has" : checkPluralOrder(item.toLowerCase())));
+                    if(orderAmount == 1){
+                        SESSION_ACTIONS.add("Ordered "+orderAmount+" "+formattedItem(item)+".");
+                    }else {
+                        if (checkPluralName(item.toLowerCase())) {
+                            SESSION_ACTIONS.add("Ordered " + orderAmount + " " + formattedItem(item) + ".");
+                        } else {
+                            SESSION_ACTIONS.add("Ordered " + orderAmount + " " + formattedItem(item) + "s.");
+                        }
+                    }
+                }
             }
+        }
+    }
+
+    private String formattedItem(String itemName) {
+        return itemName.toUpperCase().charAt(0) + itemName.substring(1).toLowerCase();
+    }
+
+    private boolean checkPluralName(String itemName) {
+        return itemName.endsWith("s");
+    }
+
+    private String checkPluralOrder(String itemName){
+        if(checkPluralName(itemName)){
+            return " have";
+        }else{
+            return "s have";
         }
     }
 
@@ -309,6 +421,8 @@ public class TheWarehouseManager extends Repository {
         printCategoryMenu(menu);
         int choice = chooseCategory();
         printCategoryItems(choice, menu);
+
+        SESSION_ACTIONS.add("Browsed the category "+menu.get(choice));
     }
 
 
@@ -326,19 +440,28 @@ public class TheWarehouseManager extends Repository {
         return result;
     }
 
-    private void printCategoryMenu(Map<Integer, String> x){
-        for(var entry : x.entrySet()) {
+    private void printCategoryMenu(Map<Integer, String> menu){
+        for(var entry : menu.entrySet()) {
             System.out.printf("%d. %s (%d)%n", entry.getKey(), entry.getValue(), amountOfItemsPerCategory(entry.getValue()));
         }
     }
+
+
 
     private int chooseCategory(){
         int result = 0;
         do{
             System.out.print("Type the number of the category to browse: ");
+            while(!reader.hasNextInt()) { //repeat until a number is entered.
+                String input = reader.nextLine();
+                System.out.println("\n**************************************************\n" +
+                        input + " is not a valid operation. Please enter a number between 1 and " + getCategories().size()+"!\n" +
+                        "**************************************************");
+                System.out.print("Type the number of the category to browse: ");
+            }
             result = Integer.parseInt(this.reader.nextLine());
             if(result < 1 || result > getCategories().size()){
-                System.out.println("You have typed an incorrect number.");
+                System.out.println("Please enter a number between 1 and "+getCategories().size()+"!");
             }
         }while(result < 1 || result > getCategories().size());
         return result;
